a79d00359c3c627f7b68150239b99f215acd77c9
Long <zz7zz7zz.zz@hotmail.com>
2025-03-18 22:22:26 +0800

    add

--- tasks/googleReviewTask.go
+++ tasks/googleReviewTask.go
@@ -3,14 +3,13 @@ package tasks
 import (
 	"fmt"
 	"net/http"
-	"strings"
+	"regexp"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 const (
-	playStoreURL = "https://play.google.com/store/apps/details?id=com.inhobichat.hobichat"                                               // 替换为你的应用 Play Store URL
-	userAgents   = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" // 随机 User-Agen
+	playStoreURL = "https://play.google.com/store/apps/details?id=com.inhobichat.hobichat" // 替换为你的应用 Play Store URL
 )
 
 type GoogleReviewTask struct {
@@ -27,7 +26,7 @@ func (t *GoogleReviewTask) Run() {
 	if err != nil {
 		fmt.Printf("抓取 Google Play 页面失败: %v", err)
 	} else {
-		fmt.Printf("当前版本ppp: %s\n", version)
+		fmt.Printf("当前版本: %s\n", version)
 	}
 }
 
@@ -45,20 +44,46 @@ func scrapePlayStore() (string, error) {
 		return "", fmt.Errorf("解析 HTML 失败: %v", err)
 	}
 
-	// 提取版本信息
-	// version := doc.Find(".hAyfc .htlgb").First().Text()
-	version := ""
-	doc.Find(".hAyfc").Each(func(i int, s *goquery.Selection) {
-		if strings.Contains(s.Text(), "Current Version") {
-			version = s.Find(".htlgb").First().Text()
-		}
-	})
+	html := doc.Text()
+	// // 测试数据
+	// html := `
+	// 	<div>版本列表：</div>
+	// 	[[["1.2.3"]]
+	// 	[[["12.3.45"]]
+	// 	[[["2.15.8"]]
+	// 	无效格式：[[["123.4.5"]]  [[["a.b.c"]] [[["1.2.3.4"]]
+	// `
 
-	if version == "" {
+	// 提取版本信息
+	versions := extractVersions(html)
+	fmt.Println("提取到的版本号：")
+	for _, v := range versions {
+		fmt.Println(v)
+	}
+
+	if len(versions) == 0 {
 		return "", fmt.Errorf("未找到版本信息")
 	}
 
 	// 清理版本字符串
-	version = strings.TrimSpace(version)
-	return version, nil
+	return versions[0], nil
+}
+
+func extractVersions(html string) []string {
+	// 正则表达式说明：
+	// \\[\\[\\[\"   匹配固定开头[[["
+	// (\\d{1,2}\\.\\d{1,2}\\.\\d{1,2}) 捕获组匹配版本号格式
+	// \"\\]\\]    匹配固定结尾"]]
+	re := regexp.MustCompile(`\[\[\[\"(\d{1,2}\.\d{1,2}\.\d{1,2})\"\]\]`)
+
+	matches := re.FindAllStringSubmatch(html, -1)
+	versions := make([]string, 0, len(matches))
+
+	for _, match := range matches {
+		if len(match) >= 2 {
+			versions = append(versions, match[1])
+		}
+	}
+
+	return versions
 }
