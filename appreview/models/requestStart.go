9f06f8d09852917caf2def43cbcb6d1f87eda50e
Long <zz7zz7zz.zz@hotmail.com>
2025-03-26 23:00:03 +0800

    9.997

--- appreview/models/requestStart.go
+++ appreview/models/requestStart.go
@@ -1,8 +1,16 @@
 package models
 
 type RequestStart struct {
-	Ver      string `json:"ver"`
-	Pkg      string `json:"pkg"`
-	Platform string `json:"platform"`
-	Status   string `json:"status"`
+	Ver      string `json:"ver" form:"ver" binding:"required"`
+	Pkg      string `json:"pkg" form:"pkg" binding:"required"`
+	Platform string `json:"platform" form:"platform" binding:"required,oneof=web ios android"`
+	Status   string `json:"status" form:"status" binding:"omitempty"`
 }
+
+type Platform string
+
+const (
+	PlatformWeb     Platform = "web"
+	PlatformIOS     Platform = "ios"
+	PlatformAndroid Platform = "android"
+)
