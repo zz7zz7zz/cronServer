6810614c1d86288aa7bdaee1c6997a3cd0d0ad63
Long <zz7zz7zz.zz@hotmail.com>
2025-03-30 17:59:56 +0800

    9.9992

--- cmd/root.go
+++ cmd/root.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -14,20 +13,24 @@ var rootCmd = &cobra.Command{
 	Long:  "long desc",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("root cmd run begins")
-
-		fmt.Println("source", cmd.Flags().Lookup("source").Value)
-		fmt.Println("dir_sqls_1", cmd.PersistentFlags().Lookup("dir_sqls_1").Value)
-		fmt.Println("dir_sqls_2", cmd.PersistentFlags().Lookup("dir_sqls_2").Value)
-		fmt.Println("dir_sqls_3", cmd.PersistentFlags().Lookup("dir_sqls_3").Value)
-		fmt.Println("dir_sqls_4", cmd.PersistentFlags().Lookup("dir_sqls_4").Value)
+		fmt.Println("-------------------cmd values-----------------------")
+		fmt.Println("paramA", cmd.PersistentFlags().Lookup("paramA").Value)
+		fmt.Println("paramB", cmd.PersistentFlags().Lookup("paramB").Value)
+		fmt.Println("paramC", cmd.PersistentFlags().Lookup("paramC").Value)
 		fmt.Println("cfg", cmd.PersistentFlags().Lookup("cfg").Value)
-		fmt.Println("------------------------------------------")
-		fmt.Println("source", viper.GetString("source"))
-		fmt.Println("dir_sqls_1", viper.GetString("dir_sqls_1"))
-		fmt.Println("dir_sqls_2", viper.GetString("dir_sqls_2"))
-		fmt.Println("dir_sqls_3", viper.GetString("dir_sqls_3"))
-		fmt.Println("dir_sqls_4", viper.GetString("dir_sqls_4"))
+		fmt.Println("database.host", cmd.PersistentFlags().Lookup("database.host").Value)
+		fmt.Println("database.port", cmd.PersistentFlags().Lookup("database.port").Value)
+		fmt.Println("env", cmd.Flags().Lookup("env").Value)
+
+		fmt.Println("-------------------viper values-----------------------")
+		fmt.Println("paramA", viper.GetString("paramA"))
+		fmt.Println("paramB", viper.GetString("paramB"))
+		fmt.Println("paramC", viper.GetString("paramC"))
 		fmt.Println("cfg", viper.GetString("cfg"))
+		fmt.Println("database.host", viper.GetString("database.host"))
+		fmt.Println("database.port", viper.GetString("database.port"))
+		fmt.Println("env", viper.GetString("env"))
+		fmt.Println("------------------------------------------")
 
 		fmt.Println("root cmd run ends")
 	},
@@ -37,50 +40,62 @@ func Execute() {
 	rootCmd.Execute()
 }
 
-var dir_sqls_3 string
-var dir_sqls_4 string
+var paramC string
+var cfgPath string
 
 func init() {
 
 	cobra.OnInitialize(InitConfig)
 
 	//按名称接收命令行参数
-	rootCmd.PersistentFlags().String("dir_sqls_1", "null", "dir_sqls_1 usage")
+	rootCmd.PersistentFlags().String("paramA", "", "paramA usage")
 	//指定flag缩写
-	rootCmd.PersistentFlags().StringP("dir_sqls_2", "a", "null", "dir_sqls2 usage")
+	rootCmd.PersistentFlags().StringP("paramB", "b", "", "paramB usage")
 	//通过指针将值赋值到字段
-	rootCmd.PersistentFlags().StringVar(&dir_sqls_3, "dir_sqls_3", "null", "dir_sqls3 usage")
+	rootCmd.PersistentFlags().StringVar(&paramC, "paramC", "", "paramC usage")
 	//通过指针将值赋值到字段，指定flag缩写
-	rootCmd.PersistentFlags().StringVarP(&dir_sqls_4, "dir_sqls_4", "b", "null", "dir_sqls4 usage")
+	rootCmd.PersistentFlags().StringVarP(&cfgPath, "cfg", "c", "", "cfg usage")
 
-	rootCmd.PersistentFlags().String("cfg", "null", "cfg usage")
+	rootCmd.PersistentFlags().String("database.host", "", "database.host")
+	rootCmd.PersistentFlags().String("database.port", "", "database.port")
 
 	//添加本地标识
-	rootCmd.Flags().StringP("source", "s", "", "source usage")
+	rootCmd.Flags().StringP("env", "e", "debug", "environment usage")
 
-	viper.BindPFlag("dir_sqls_1", rootCmd.PersistentFlags().Lookup("dir_sqls_1"))
-	viper.BindPFlag("dir_sqls_2", rootCmd.PersistentFlags().Lookup("dir_sqls_2"))
-	viper.BindPFlag("dir_sqls_3", rootCmd.PersistentFlags().Lookup("dir_sqls_3"))
-	viper.BindPFlag("dir_sqls_4", rootCmd.PersistentFlags().Lookup("dir_sqls_4"))
+	fmt.Println("-------------------viper bindP-----------------------")
+
+	viper.BindPFlag("paramA", rootCmd.PersistentFlags().Lookup("paramA"))
+	viper.BindPFlag("paramB", rootCmd.PersistentFlags().Lookup("paramB"))
+	viper.BindPFlag("paramC", rootCmd.PersistentFlags().Lookup("paramC"))
 	viper.BindPFlag("cfg", rootCmd.PersistentFlags().Lookup("cfg"))
+	viper.BindPFlag("database.host", rootCmd.PersistentFlags().Lookup("database.host"))
+	viper.BindPFlag("database.port", rootCmd.PersistentFlags().Lookup("database.port"))
+	viper.BindPFlag("env", rootCmd.Flags().Lookup("env"))
 
-	viper.SetDefault("dir_sqls_1", "d_s_1")
-	viper.SetDefault("dir_sqls_2", "d_s_2")
-	viper.SetDefault("dir_sqls_3", "d_s_3")
-	viper.SetDefault("dir_sqls_4", "d_s_4")
-	viper.SetDefault("cfg", "cfg")
-
+	viper.SetDefault("paramA", "viper default paramA")
+	viper.SetDefault("paramB", "viper default paramB")
+	viper.SetDefault("paramC", "viper default paramC")
+	viper.SetDefault("cfg", "viper default cfg")
+	viper.SetDefault("database.host", "viper default 127.0.0.111")
+	viper.SetDefault("database.port", "viper default 88")
+	viper.SetDefault("env", "viper default env")
 }
 
 func InitConfig() {
-	if dir_sqls_3 != "" {
-		viper.SetConfigFile(dir_sqls_3)
+	if cfgPath != "" {
+		fmt.Println("user cfg file")
+		viper.SetConfigFile(cfgPath)
+
 	} else {
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-		viper.AddConfigPath(home)
+		fmt.Println("user default file")
+		// home, err :=
+		// cobra.CheckErr(err)
+		// viper.AddConfigPath(home)
+		// fmt.Println("home", home)
+
+		viper.AddConfigPath(".")
 		viper.SetConfigType("yaml")
-		viper.SetConfigName(".cobra")
+		viper.SetConfigName("config")
 	}
 	//检查环境变量，将配置的键值加载到viper
 	viper.AutomaticEnv()
--- readme.txt
+++ readme.txt
@@ -1,3 +1,4 @@
 protoc --go_out=.  appreview.proto 
 
 go run main.go --dir_sqls_1=d1 --dir_sqls_2=d2 --dir_sqls_3=d3 -b=d4 -s=sourcevalue
+go run main.go --cfg config.yaml
